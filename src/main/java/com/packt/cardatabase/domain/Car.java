3d2aaa2078a2e11f2f703d76f3ca9aa492b5331b
Omkar Kotiwale <omkarkothiwale@gmail.com>
2023-03-14 22:21:36 +0530

    H2 db queries with different JPA methods

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,9 +1,8 @@
 package com.packt.cardatabase.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Car {
@@ -13,18 +12,21 @@ public class Car {
     private long id;
     private String brand, model, color, registerNumber;
     private int year, price;
+    @ManyToMany(mappedBy = "cars")
+    private Set<Owner> owner =  new HashSet<>();
 
     public Car () {
 
     }
 
-    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+    public Car(String brand, String model, String color, String registerNumber, int year, int price, Set<Owner> owner) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.registerNumber = registerNumber;
         this.year = year;
         this.price = price;
+        this.owner = owner;
     }
 
     public long getId() {
@@ -73,6 +75,14 @@ public class Car {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public Set<Owner> getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Set<Owner> owner) {
+        this.owner = owner;
     }
 
     public int getPrice() {
